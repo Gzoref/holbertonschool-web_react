cf834b92667b6d76aaf5ffef25556db4b8bf7414
Gzoref <gzoref@gmail.com>
2021-02-11 16:45:52 -0500

    Advanced types Part 1

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,47 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+    workFromHome() {
+        return 'Working from home';
+    }
+
+    getCoffeeBreak() {
+        return 'Getting a coffee break';
+    }
+
+    workDirectorTasks() {
+        return 'Getting to director tasks';
+    }
+}
+
+export class Teacher implements TeacherInterface {
+
+    workFromHome() {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak() {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks() {
+        return 'Getting to work';
+    }
+}
+
+export function createEmployee(salary: number | string) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
