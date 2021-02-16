1b24d23fc9ebbcc781afff3d64101548b01e9e19
Gzoref <gzoref@gmail.com>
2021-02-16 18:36:43 -0500

    Remove quotes round html tags

--- 0x02-react_intro/task_1/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_1/dashboard/src/Notifications.js
@@ -2,5 +2,9 @@ import React from 'react';
 import './Notifications.css'
 
 export function notification() {
-    return '<div classname="Notifications"><p>Here is the list of notifications</p></div>'
+    return (
+    <div classname="Notifications">
+        <p>Here is the list of notifications</p>
+    </div>
+    );
 }
