f13589593b586d33a8175cf43915eb7846e1975c
Gzoref <gzoref@gmail.com>
2021-02-18 18:06:32 -0500

    Install babel

--- 0x02-react_intro/task_5/dashboard/dist/index.js
+++ 0x02-react_intro/task_5/dashboard/dist/index.js
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App';
-import Notifications from './Notifications/Notifications';
+import App from './src/App/App';
+import Notifications from './src/Notifications/Notifications';
 
 ReactDOM.render(
     <React.StrictMode>
