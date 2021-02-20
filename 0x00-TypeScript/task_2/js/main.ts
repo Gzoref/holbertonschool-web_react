3a9e6d68d69b3ba41a4e55c8ce186f9053723c72
Gzoref <gzoref@gmail.com>
2021-02-20 17:19:12 -0500

    Remove commented tests

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -56,9 +56,9 @@ type Subjects = 'Math' | 'History';
 
 function teachClass(todayClass: Subjects): string {
     if (todayClass === 'Math') {
-        return "Teaching Math";
+        return 'Teaching Math';
     } else if (todayClass === 'History') {
-        return "Teaching History";
+        return 'Teaching History';
     }
 }
 /*
