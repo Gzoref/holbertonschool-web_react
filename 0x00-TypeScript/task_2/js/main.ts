5ff3ac8a481c3499aa45ef1ebc3a0e23e7034795
Gzoref <gzoref@gmail.com>
2021-02-20 16:36:32 -0500

    Eslint Fixes

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -10,7 +10,7 @@ interface TeacherInterface {
     workTeacherTasks(): string;
 }
 
-export class Director implements DirectorInterface {
+class Director implements DirectorInterface {
     workFromHome() {
         return 'Working from home';
     }
@@ -24,7 +24,7 @@ export class Director implements DirectorInterface {
     }
 }
 
-export class Teacher implements TeacherInterface {
+class Teacher implements TeacherInterface {
 
     workFromHome() {
         return 'Cannot work from home';
@@ -39,18 +39,18 @@ export class Teacher implements TeacherInterface {
     }
 }
 
-export function createEmployee(salary: number | string) {
+function createEmployee(salary: number | string) {
     if (typeof salary === 'number' && salary < 500) {
         return new Teacher();
     }
     return new Director();
 }
 
-export function isDirector(employee: DirectorInterface | TeacherInterface) {
+function isDirector(employee: DirectorInterface | TeacherInterface) {
     return (employee as Director).workDirectorTasks !== undefined;
 }
 
-export function executeWork(employee: DirectorInterface | TeacherInterface) {
+function executeWork(employee: DirectorInterface | TeacherInterface) {
     if (employee instanceof Director) {
         return employee.workDirectorTasks();
     } else if (employee instanceof Teacher) {
@@ -58,12 +58,12 @@ export function executeWork(employee: DirectorInterface | TeacherInterface) {
     }
 }
 
-type Subjects = "Math" | "History";
+type Subjects = 'Math' | 'History';
 
-export function teachClass(todayClass: Subjects): string{
-    if (todayClass === "Math") {
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
         return "Teaching Math";
-    } else if (todayClass === "History") {
+    } else if (todayClass === 'History') {
         return "Teaching History";
     }
 }
