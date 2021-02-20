2c5095ef4e46dbb29f43dc4e9f949494c5f4c09b
Gzoref <gzoref@gmail.com>
2021-02-20 18:35:08 -0500

    Brand convention & Nominal typing

---
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -0,0 +1,17 @@
+interface MajorCredits {
+    credits: number,
+    brand: "MajorCredits",
+}
+
+interface MinorCredits {
+    credits: number
+    brand: "MinorCredits",
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+    return { credit: subject1.credits + subject2.credits, brand: "MajorCredits" }
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
+    return { credit: subject1.credits + subject2.credits, brand: "MinorCredits" }
+}
