d499fa93454ffcb602f8f4d1eeb64ed986c2e8b0
Gzoref <gzoref@gmail.com>
2021-02-20 16:39:23 -0500

    Eslint Fixes

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,66 +1,60 @@
 interface DirectorInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string;
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workDirectorTasks(): string,
 }
-
 interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workTeacherTasks(): string,
 }
 
-class Director implements DirectorInterface {
-    workFromHome() {
-        return 'Working from home';
+export class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
     }
-
-    getCoffeeBreak() {
-        return 'Getting a coffee break';
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
     }
-
-    workDirectorTasks() {
-        return 'Getting to director tasks';
+    workDirectorTasks(): string {
+        return "Getting to director tasks"
     }
 }
-
-class Teacher implements TeacherInterface {
-
-    workFromHome() {
-        return 'Cannot work from home';
+export class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home";
     }
-
-    getCoffeeBreak() {
-        return 'Cannot have a break';
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
     }
-
-    workTeacherTasks() {
-        return 'Getting to work';
+    workTeacherTasks(): string {
+        return "Getting to work"
     }
 }
 
-function createEmployee(salary: number | string) {
-    if (typeof salary === 'number' && salary < 500) {
+export const createEmployee = function (salary: number | string): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
         return new Teacher();
+    } else {
+        return new Director();
     }
-    return new Director();
 }
 
-function isDirector(employee: DirectorInterface | TeacherInterface) {
-    return (employee as Director).workDirectorTasks !== undefined;
+export function isDirector(employee: DirectorInterface | TeacherInterface): boolean {
+    return (employee instanceof Director);
 }
 
-function executeWork(employee: DirectorInterface | TeacherInterface) {
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
     if (employee instanceof Director) {
-        return employee.workDirectorTasks();
+        return (employee.workDirectorTasks());
     } else if (employee instanceof Teacher) {
-        return employee.workTeacherTasks();
+        return (employee.workTeacherTasks());
     }
 }
 
 type Subjects = 'Math' | 'History';
 
-function teachClass(todayClass: Subjects): string {
+export function teachClass(todayClass: Subjects): string {
     if (todayClass === 'Math') {
         return "Teaching Math";
     } else if (todayClass === 'History') {
